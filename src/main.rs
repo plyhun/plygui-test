f6079b9ca021c572b9f51e9fa7ab934125068b7a
Serhii Plyhun <snuk188@gmail.com>
2019-04-27 16:31:33 +0200

    Make UI thread main

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,36 @@
 use plygui::Window;
 
-use std::sync::{Arc, RwLock, atomic::{Ordering, AtomicBool}};
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    Arc, RwLock,
+};
 use std::thread;
 use std::time::Duration;
 
 mod lib;
 
 fn main() {
-    let feeders = Arc::new(RwLock::new(Vec::new()));
+    let feeders: Arc<RwLock<Vec<plygui::callbacks::AsyncFeeder<plygui::callbacks::OnFrame>>>> = Arc::new(RwLock::new(Vec::new()));
     let feeders2 = feeders.clone();
     let running = Arc::new(AtomicBool::new(true));
     let running2 = running.clone();
-    
-    thread::spawn(move ||{
-        lib::exec(feeders2);
-        running2.store(false, Ordering::SeqCst);
-    });
-    
-    while running.load(Ordering::SeqCst) {
-        thread::sleep(Duration::from_millis(5000));
-        
-        if feeders.write().unwrap().len() > 0 {
-            let _ = feeders.write().unwrap().remove(0).feed((move |w: &mut dyn (Window)| { 
-                        w.close(true); 
-                        false 
-                }).into());
+
+    thread::spawn(move || {
+        while running.load(Ordering::SeqCst) {
+            thread::sleep(Duration::from_millis(5000));
+
+            if feeders2.write().unwrap().len() > 0 {
+                let _ = feeders2.write().unwrap().remove(0).feed(
+                    (move |w: &mut dyn (Window)| {
+                        w.close(true);
+                        false
+                    })
+                    .into(),
+                );
+            }
         }
-    }
+    });
+
+    lib::exec(feeders);
+    running2.store(false, Ordering::SeqCst);
 }
